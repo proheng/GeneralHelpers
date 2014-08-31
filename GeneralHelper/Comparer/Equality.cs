f36f7710920b937d0b4b6e98094677a7792ed449
Rex Zhiheng He <rex@surprisingme.net>
2014-08-31 22:59:13 +1000

    init commit

---
+++ GeneralHelper/Comparer/Equality.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GeneralHelper.Comparer
+{
+    public class Equality
+    {
+        public static bool EqualsDefaultValue<T>(T value)
+        {
+            return EqualityComparer<T>.Default.Equals(value, default(T));
+        }
+    }
+}
---
+++ GeneralHelperTests/Comparer/EqualityTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GeneralHelper.Comparer;
+using Xunit;
+
+namespace GeneralHelperTests.Comparer
+{
+    public class EqualityTests
+    {
+        [Fact]
+        public void TestEqualsDefaultValue()
+        {
+            bool defaultBool = new bool();
+            Assert.True(Equality.EqualsDefaultValue<bool>(defaultBool)); 
+
+            char defaultChar = new char();
+            Assert.True(Equality.EqualsDefaultValue<char>(defaultChar));
+
+            int defaultInt = new int();
+            Assert.True(Equality.EqualsDefaultValue<int>(defaultInt));
+
+            DateTime defaultDateTime = new DateTime();
+            Assert.True(Equality.EqualsDefaultValue<DateTime>(defaultDateTime));
+
+        }
+    }
+}
